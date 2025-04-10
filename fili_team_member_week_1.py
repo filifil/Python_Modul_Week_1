c9659724a56df77b4d5f708e9a8e9967367c9fcc
filifili <filifilibro@gmail.com>
2025-04-10 08:28:57 +0200

    question 2 is done

--- fili_team_member_week_1.py
+++ fili_team_member_week_1.py
@@ -1,3 +1,14 @@
-#Python_Modul_Week_1_question_1
-for i in range(1,11):
+#input from the user
+num = int(input("Enter a number: "))
+
+for i in range(2, num + 1, 2):  
+# Print every even number    
     print(i)
+
+num = int(input("Enter a number: "))
+
+print("Even numbers whith while loop:")
+i = 2  # Start from 2
+while i <= num:
+    print(i)
+    i += 2  # Increasing  by 2     
